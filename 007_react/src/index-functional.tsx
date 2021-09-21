f3334399efc2f06f3e3a9df3a9787db86997bcd4
mstanka <mice78.mm@gmail.com>
2021-09-21 16:17:35 +0200

    add react project

---
+++ 007_react/src/index-functional.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+interface AppProps {
+  color?: string;
+}
+
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
+
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
